8faa379e34935efcf9be4fad362592bcfa7eb05a
jbeans0312 <jbeans@udel.edu>
2022-09-22 16:09:33 +0000

    Added basic exit functionality to ushell.c, have not tested yet.

--- ushell.c
+++ ushell.c
@@ -1,5 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
 
-//Makes a shell and runs the program and turns it in for us and also writes all our code and gives us an A
-return(shell);
+#define MAXBUFFER 128
+
+int main(void){
+	char buffer[MAXBUFFER];	
+	//Note to self: initially malloc prompt then use 'realloc' later when prompt is changed
+	char* prefix = malloc(2 * sizeof(char));
+	strcpy(prefix, " ", 2);
+	
+	char* wdpath = NULL;
+	int pathlen = (int) strlen(getcwd(NULL, 0)) + 1;
+	wdpath = malloc(pathlen * sizeof(char));
+	strcpy(wdpath, getcwd(NULL, 0), pathlen);
+
+	int running = 1;
+	
+
+	do {
+		printf("%s[%s]>", prefix, wdpath);
+		if(fgets(buffer, MAXBUFFER, stdin) != NULL){
+			int inputlen = strlen(buffer);
+
+			if(buffer[inputlen - 1] == '\n'){ //replace newline char with null char
+				buffer[inputlen - 1] = '\0';
+			}	
+			if(strcmp(buffer, "exit" == 0){
+				free(prefix); //replace with a call to a memory freeing function?
+				free(wdpath);
+				running = 0;
+			}
+		}
+	}while(running);
+
+	exit(1);
+}
