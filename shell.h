557cc3ad54924da1dc6e04d68608f64989238699
jbeans0312 <jbeans0312@udel.edu>
2022-09-27 15:54:01 +0000

    @jbeans0312 added the PathElement struct to shell.h

--- shell.h
+++ shell.h
@@ -1,3 +1,13 @@
+
+//Defines the PathElement struct which is used to build the linked list that
+//represents the path to the current working drectory
+struct PathElement;
+
+typedef struct PathElement{
+	char* dir_name; 
+	struct PathElement* next;
+}PathElement;
+
 //Params: char* prefix and char* wdpath
 //Frees the memory allocated to the prefix and the memory allocated to the working directory strings
 //Exits the shell
