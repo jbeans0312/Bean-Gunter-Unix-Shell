6e41a0f1a3acbdcecd17387b63aab15c5ad60bd7
Will Gunter <wgunter@udel.edu>
2022-09-28 23:45:45 +0000

    Update code to read input -> break into args array

--- shell.h
+++ shell.h
@@ -12,10 +12,11 @@ typedef struct PathElement{
 //Frees the memory allocated to the prefix and the memory allocated to the working directory strings
 //Exits the shell
 //Returns: None
-void exit_ush(char* pf, char* wp);
+void exit_ush(char* pf, char* wp, char* args);
 
 //Function that implements the functionality of the pid command - "prints the pid of the shell"
 //Params: None
 //Returns: the pid as the datatype pid_t
 pid_t get_pid();
 
+//Function that 
--- ushell.c
+++ ushell.c
@@ -6,6 +6,7 @@
 #include <shell.h>
 
 #define MAXBUFFER 128
+#define MAXARGS 10
 
 int main(void){
 	char buffer[MAXBUFFER];	
@@ -17,6 +18,8 @@ int main(void){
 	wdpath = getcwd(NULL, 0);
 
 	int running = 1;
+
+	char **args=malloc(MAXARGS*sizeof(char*));
 	
 
 	do {
@@ -28,11 +31,25 @@ int main(void){
 				buffer[inputlen - 1] = '\0';
 			}
 
-			if(strcmp(buffer, "exit") == 0){ //calls the exit function
-				exit_ush(prefix, wdpath);
+			//Break the buffer into an array of strings broken up by the space character
+			//First, we need to clear the args array in case it was used in a previous iteration
+			//As a result, args[0] will be the command being run and args[1] will be the first argument
+			for(int i = 0; i < MAXARGS; i++){
+				args[i] = NULL;
+			}
+			int i = 0;
+			char* token = strtok(buffer, " ");
+			while(token != NULL){
+				args[i] = token;
+				token = strtok(NULL, " ");
+				i++;
+			}
+
+			if(strcmp(args[0], "exit") == 0){ //calls the exit function
+				exit_ush(prefix, wdpath, args);
 			}	
 
-			if(strcmp(buffer, "pid") == 0){ //calls the pid function
+			if(strcmp(args[0], "pid") == 0){ //calls the pid function
 				printf("pid: %d\n", get_pid());
 			}
 
@@ -40,12 +57,13 @@ int main(void){
 	}while(running);
 }
 
-//Frees the memory allocated to prefix and wdpath
+//Frees the memory allocated to prefix, wdpath, and args
 //Exits the shell
-//Params: char* prefix, char* wdpath
-void exit_ush(char* pf, char* wd){
+//Params: char* prefix, char* wdpath, char** args
+void exit_ush(char* pf, char* wd, char* args){
 	free(pf);
 	free(wd);
+	free(args);
 	exit(1);
 }
 
