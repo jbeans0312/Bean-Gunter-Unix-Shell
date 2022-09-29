a9e6f90d730b50f1a280b0b24b0bf48011757624
Will Gunter <wgunter@udel.edu>
2022-09-29 00:49:45 +0000

    Implement printenv command

--- shell.h
+++ shell.h
@@ -25,3 +25,11 @@ pid_t get_pid();
 //Params: char** args[]
 //Returns: an integer representing the status of the kill command - 0 means failure, 1 means success
 int cmd_kill(char** args);
+
+//Function that implements the functionality of the printenv command - "prints the value of an environment variable"
+//If there are no arguments, the function will print all the environment variables and their values
+//If there is one argument, the function will print the value of the environment variable specified by the argument
+//First index in the args array is the command name, second index is the first argument, etc.
+//Params: char** args[]
+//Returns: an integer representing the status of the printenv command - 0 means failure, 1 means success
+int cmd_printenv(char** args);
--- ushell.c
+++ ushell.c
@@ -59,6 +59,10 @@ int main(void){
 				cmd_kill(args);
 			}
 
+			if(strcmp(args[0], "printenv") == 0){ //calls the printenv function
+				cmd_printenv(args);
+			}
+
 		}
 	}while(running);
 }
@@ -120,4 +124,28 @@ int cmd_kill(char** args) {
 		status = 0;
 	}
 	return status;
+}
+
+//Function that implements the functionality of the printenv command - "prints the value of an environment variable"
+//If there are no arguments, the function will print all the environment variables and their values
+//If there is one argument, the function will print the value of the environment variable specified by the argument
+//First index in the args array is the command name, second index is the first argument, etc.
+//Params: char** args[]
+//Returns: an integer representing the status of the printenv command - 0 means failure, 1 means success
+int cmd_printenv(char** args) {
+	int status = 0;
+	if(args[1] == NULL) {
+		for(int i = 0; environ[i] != NULL; i++) {
+			printf("%s\n", environ[i]);
+		}
+	} else if(args[2] == NULL) {
+		char* env = getenv(args[1]);
+		if(env != NULL) {
+			printf("%s\n", env);
+		} else {
+			printf("printenv: %s: no such variable\n", args[1]);
+		}
+	} else {
+		printf("printenv: too many arguments\n");
+	}
 }
